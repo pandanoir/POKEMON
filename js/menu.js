f0e35d1b479d6016298027fe2c12ae63323a494b
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2016-08-10 22:03:52 +0900

    remove needless codes in menu.js.

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,34 @@
 var menuList = ['pokemon', 'pokedex', 'status', 'setting', 'save'];
-window.addEventListener('load', function() {
-    var movesCursor = 0;
-    setupCanvas();
-    drawMenu();
-    drawMap();
-    drawCanvas();
-    mainLoop.add(function() {
-        if (sceen === 'MENU') {
-            setupCanvas();
-            drawMap();
-            drawMenu();
-            drawCanvas();
-            if (pressedKey.up > 0 && movesCursor === 0 && cursor.y - 1 >= 0) {
-                cursor.y--;
-                movesCursor = 1;
-            }
-            if (pressedKey.down > 0 && movesCursor === 0 ) {
-                cursor.y++;
-                movesCursor = 1;
-            }
-            if (movesCursor !== 0) {
-                movesCursor++;
-            }
-            if (movesCursor === 11) {
-                movesCursor = 0;
-            }
-            if (pressedKey.space === 1) {
-                if (menuList[cursor.y] === 'pokedex') {
-                    changeSceen('POKEDEX_INDEX');
-                } else {
-                    changeSceen('MAP');
-                }
-            }
-            if (pressedKey.M === 1 || pressedKey.B === 1) {
+var movesCursor = 0;
+mainLoop.add(function() {
+    if (sceen === 'MENU') {
+        setupCanvas();
+        drawMap();
+        drawMenu();
+        drawCanvas();
+        if (pressedKey.up > 0 && movesCursor === 0 && cursor.y - 1 >= 0) {
+            cursor.y--;
+            movesCursor = 1;
+        }
+        if (pressedKey.down > 0 && movesCursor === 0 && cursor.y + 1 <= menuList.length - 1) {
+            cursor.y++;
+            movesCursor = 1;
+        }
+        if (movesCursor !== 0) {
+            movesCursor++;
+        }
+        if (movesCursor === 11) {
+            movesCursor = 0;
+        }
+        if (pressedKey.space === 1) {
+            if (menuList[cursor.y] === 'pokedex') {
+                changeSceen('POKEDEX_INDEX');
+            } else {
                 changeSceen('MAP');
             }
         }
-    });
+        if (pressedKey.M === 1 || pressedKey.B === 1) {
+            changeSceen('MAP');
+        }
+    }
 });
