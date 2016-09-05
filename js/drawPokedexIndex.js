ba8b6daff009587418ffab2bc83fc10918aeb943
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2016-09-05 09:42:02 +0900

    performance improve in drawPokedexIndex.js

--- js/drawPokedexIndex.js
+++ js/drawPokedexIndex.js
@@ -1,12 +1,14 @@
-import {UPoint, UText, pokedex, pokemonList, lineHeight, buffer, zerofill} from './definitions.js';
+import {UPoint, UText, pokedex, pokemonList, lineHeight, canvasHeight, buffer, zerofill} from './definitions.js';
 
 export default () => {
     const cursorText = newUText('>', new UPoint(0, (pokedex.cursor.y - pokedex.pageYOffset) * lineHeight));
 
     buffer.add(cursorText);
     for (let i = 0, _i = pokemonList.length; i < _i; i++) {
-        buffer.add(newUText('No ' + zerofill(i + 1, 3), new UPoint(lineHeight, (i - pokedex.pageYOffset) * lineHeight)));
-        buffer.add(newUText(pokemonList[i].name, new UPoint(lineHeight * 6, (i - pokedex.pageYOffset) * lineHeight)));
+        const Y = (i - pokedex.pageYOffset) * lineHeight;
+        if (Y >= canvasHeight || -2 * lineHeight >= Y) continue;
+        buffer.add(newUText('No ' + zerofill(i + 1, 3), new UPoint(lineHeight, Y)));
+        buffer.add(newUText(pokemonList[i].name, new UPoint(lineHeight * 6, Y)));
     }
     function newUText(string, point) {
         return new UText(string, point).setFillColor('#555').setBaseline('top').setFont('13px san-self');
